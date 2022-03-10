93fb092c904d860b6ac2c5912ba3a0ec88227a90
neomagic100 <neomagic100@gmail.com>
2022-03-10 12:42:05 -0500

    Update Create_Tables.sql
    Added tables to now include University, Users, Admins, Superusers, Events.
    
    Primary keys (ids) are auto-incremented integers

--- Create_Tables.sql
+++ Create_Tables.sql
@@ -1,10 +1,61 @@
 USE UniversityEvents;
 
+-- University Table
+CREATE TABLE University (
+	uni_id INTEGER auto_increment NOT NULL,
+    uni_name VARCHAR(255),
+    location VARCHAR(255),
+    num_students INTEGER,
+    descrip VARCHAR(1023),
+    PRIMARY KEY(uni_id)
+);
+
+-- Users superclass
+CREATE TABLE Users (
+	uid INTEGER auto_increment NOT NULL,
+    user_id VARCHAR(255),
+    user_pass VARCHAR(255),
+    PRIMARY KEY(uid)
+);
+
+-- Admins subclass of Users
+CREATE TABLE Admins (
+	uid INTEGER NOT NULL,
+    user_id VARCHAR(255),
+    user_pass VARCHAR(255),
+    PRIMARY KEY(uid),
+    FOREIGN KEY(uid) REFERENCES Users(uid)
+		ON DELETE CASCADE
+        ON UPDATE CASCADE
+);
+
+-- Admins subclass of Users
+CREATE TABLE Superusers (
+	uid INTEGER NOT NULL,
+    user_id VARCHAR(255),
+    user_pass VARCHAR(255),
+    PRIMARY KEY(uid),
+    FOREIGN KEY(uid) REFERENCES Users(uid)
+		ON DELETE CASCADE
+        ON UPDATE CASCADE
+);
+
 -- RSO table, have primary key as autoincrementing integer
 CREATE TABLE RSOs (
-	rso_id INTEGER AUTO_INCREMENT NOT NULL,
+	rso_id INTEGER auto_increment NOT NULL,
     rname VARCHAR(255),
     rtype VARCHAR(255),
     is_active VARBINARY(3),
     PRIMARY KEY(rso_id)
+);
+
+-- Events table
+CREATE TABLE Events (
+	event_id INTEGER auto_increment NOT NULL,
+    event_name VARCHAR(255),
+    event_category VARCHAR(255),
+    view_type VARCHAR(255),
+    is_published VARBINARY(3),
+    descrip VARCHAR(1023),
+    PRIMARY KEY(event_id)
 );
