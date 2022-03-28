accb8849ad8201f9ec49826afce041236407db16
neomagic100 <neomagic100@gmail.com>
2022-03-28 15:50:54 -0400

    Added sort in Views
    Sorts view by day and time

--- EventViews.sql
+++ EventViews.sql
@@ -1,11 +1,12 @@
 -- Templates of Views
 
 USE UniversityEvents;
-DROP VIEW PublicEventView;
+
 CREATE VIEW PublicEventView AS
 	SELECT event_name, descrip, event_date, event_start, event_end
     FROM PublicEvents
-    INNER JOIN Events USING (event_id);
+    INNER JOIN Events USING (event_id)
+    ORDER BY event_date, event_start;
     
 CREATE VIEW PrivateEventView AS
 	SELECT event_name, descrip, event_date, event_start
@@ -14,7 +15,8 @@ CREATE VIEW PrivateEventView AS
     UNION
     SELECT event_name, descrip, event_date, event_start
     FROM PrivateEvents
-    INNER JOIN Events USING (event_id);
+    INNER JOIN Events USING (event_id)
+    ORDER BY event_date, event_start;
     
 CREATE VIEW RSOEventView AS
 	SELECT event_name, descrip, event_date, event_start
@@ -27,4 +29,5 @@ CREATE VIEW RSOEventView AS
     UNION
     SELECT event_name, descrip, event_date, event_start
     FROM RSOEvents
-    INNER JOIN Events USING (event_id);
+    INNER JOIN Events USING (event_id)
+    ORDER BY event_date, event_start;
