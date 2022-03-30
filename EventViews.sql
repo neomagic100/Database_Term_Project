a78d663a2b10fc03a17053ab250682b924cb6c44
neomagic100 <neomagic100@gmail.com>
2022-03-30 15:56:15 -0400

    Updated Formatted views
    Changed dates to display as
    Mon, Feb 2, 2022
    
    Ands times as
    12:42 PM

--- EventViews.sql
+++ EventViews.sql
@@ -1,20 +1,25 @@
 USE UniversityEvents;
-
+DROP VIEW PublicEventView;
 CREATE VIEW PublicEventView AS
-	SELECT event_id, event_name, descrip, event_date, event_start, event_end
-    -- SELECT event_name, descrip, DAYNAME(event_date), MONTHNAME(event_date), DAYOFMONTH(event_date), YEAR(event_date), event_start, event_end
+	SELECT event_id, event_name, descrip, DATE_FORMAT(event_date, '%a, %b %d, %Y') AS event_date,
+		TIME_FORMAT(event_start, '%h:%i %p') AS event_start, 
+        TIME_FORMAT(event_end, '%h:%i %p') AS event_end
     FROM PublicEvents
     INNER JOIN Events USING (event_id)
     ORDER BY event_date, event_start;
     
 CREATE VIEW PrivateEventView AS
-    SELECT event_id, event_name, descrip, event_date, event_start
+    SELECT event_id, event_name, descrip, DATE_FORMAT(event_date, '%a, %b %d, %Y') AS event_date,
+		TIME_FORMAT(event_start, '%h:%i %p') AS event_start, 
+        TIME_FORMAT(event_end, '%h:%i %p') AS event_end
     FROM PrivateEvents
     INNER JOIN Events USING (event_id)
     ORDER BY event_date, event_start;
     
 CREATE VIEW RSOEventView AS
-    SELECT event_id, event_name, descrip, event_date, event_start
+    SELECT event_id, event_name, descrip, DATE_FORMAT(event_date, '%a, %b %d, %Y') AS event_date,
+		TIME_FORMAT(event_start, '%h:%i %p') AS event_start, 
+        TIME_FORMAT(event_end, '%h:%i %p') AS event_end
     FROM RSOEvents
     INNER JOIN Events USING (event_id)
     ORDER BY event_date, event_start;
