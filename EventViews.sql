d8aa3e4d9df6c19ddba3fff74c8fd05d6bfdf3c6
neomagic100 <neomagic100@gmail.com>
2022-03-30 18:19:52 -0400

    Update EventViews.sql

--- EventViews.sql
+++ EventViews.sql
@@ -1,5 +1,5 @@
 USE UniversityEvents;
-DROP VIEW PublicEventView;
+
 CREATE VIEW PublicEventView AS
 	SELECT event_id, event_name, descrip, DATE_FORMAT(event_date, '%a, %b %d, %Y') AS event_date,
 		TIME_FORMAT(event_start, '%h:%i %p') AS event_start, 
