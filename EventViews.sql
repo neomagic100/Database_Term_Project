0b8b0757f68a3036cf039c7d70ed7e54303905e2
neomagic100 <neomagic100@gmail.com>
2022-03-31 11:47:25 -0400

    comments
    added comments to sql views

--- EventViews.sql
+++ EventViews.sql
@@ -1,5 +1,9 @@
 USE UniversityEvents;
 
+-- Dates formatted as Wed, Mar 31, 2022
+-- Times formatted as 11:46 AM
+
+-- View to see public events, date and time formatted, ordered chronologically
 CREATE VIEW PublicEventView AS
 	SELECT event_id, event_name, descrip, DATE_FORMAT(event_date, '%a, %b %d, %Y') AS event_date,
 		TIME_FORMAT(event_start, '%h:%i %p') AS event_start, 
@@ -7,7 +11,8 @@ CREATE VIEW PublicEventView AS
     FROM PublicEvents
     INNER JOIN Events USING (event_id)
     ORDER BY Events.event_date, Events.event_start;
-    
+
+-- View to see private events, date and time formatted, ordered chronologically
 CREATE VIEW PrivateEventView AS
     SELECT event_id, event_name, descrip, DATE_FORMAT(event_date, '%a, %b %d, %Y') AS event_date,
 		TIME_FORMAT(event_start, '%h:%i %p') AS event_start, 
@@ -15,7 +20,8 @@ CREATE VIEW PrivateEventView AS
     FROM PrivateEvents
     INNER JOIN Events USING (event_id)
     ORDER BY Events.event_date, Events.event_start;
-    
+
+-- -- View to see RSO events, date and time formatted, ordered chronologically
 CREATE VIEW RSOEventView AS
     SELECT event_id, event_name, descrip, DATE_FORMAT(event_date, '%a, %b %d, %Y') AS event_date,
 		TIME_FORMAT(event_start, '%h:%i %p') AS event_start, 
