3fe92788cd9fdb06e14ff4d322f3827e5e5f00ee
neomagic100 <neomagic100@gmail.com>
2022-04-02 16:01:10 -0400

    Started on create university for superuser

---
+++ LAMP/API/createUniversity.php
@@ -0,0 +1,21 @@
+<?php
+    header('Access-Control-Allow-Origin: https://www.goldenknights.systems/');
+    header("Access-Control-Allow-Credentials: true");
+    header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
+    header('Access-Control-Max-Age: 1000');
+    header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token , Authorization');
+    include 'dbconfig.php';
+    $inData = getRequestInfo();
+
+    $conn = new mysqli($db_server, $db_user, $db_password, $db_name, $db_port); 
+    if (!$conn) {
+        die("Connection failed: " . mysqli_connect_error());
+    }
+
+
+
+    function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+?>
