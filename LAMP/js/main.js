a15d7888d7d20a82cd0cd10f06ea22123709388b
Adin Lukas <Lukeisun57@gmail.com>
2022-04-02 14:09:10 -0400

    Commented out the .js files

--- LAMP/js/main.js
+++ LAMP/js/main.js
@@ -1,23 +1,32 @@
+// Set up where the calls will be made to.
 var urlBase = 'https://www.goldenknights.systems/API';
 var extension = 'php';
 
 function getName()
 {
+	// In a browser session, retrieve from the key 'Name' from local storage. 
 	document.getElementById("Name").innerHTML=localStorage.getItem("Name");
 }
-function goToPublic()
-{
-	window.location.href= "publicEvents.html";
-}
+// Defunct will delete later.
+// function goToPublic()
+// {
+// 	// Redirects browser to the 
+// 	window.location.href= "publicEvents.html";
+// }
 function doLogin()
 {
 	userId = 0;
 	Name = "";
+	// In the html get the value of whatever is in the fields. (In this case whatever is in the loginName/password input.)
 	var login = document.getElementById("loginName").value;
 	var password = document.getElementById("loginPassword").value;
+	//"result" is like the error message you get (e.g. Invalid password!). This just sets it as blank. 
 	document.getElementById("result").innerHTML = "";
+	// This is what gets sent to the .php file (backend) just formatted as JSON.
 	var tmp = {Login:login,Password:password};
+	// Formats tmp from JSON into a string, since JSON is its own datatype here.
 	var jsonPayload = JSON.stringify(tmp);
+	// Where to call to? 
 	var url = urlBase + '/login.' + extension;
 
 	var xhr = new XMLHttpRequest();
@@ -25,25 +34,31 @@ function doLogin()
   	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
 	try
 	{
+		// This function happens when we get a response.
 		xhr.onreadystatechange = function() 
 		{
+			// This just tells us if the response was succesful 
 			if (this.readyState == 4 && this.status == 200) 
 			{
+				// Response text is a string, parse it and format it into a JSON datatype/
 				var jsonObject = JSON.parse( xhr.responseText );
 				if(jsonObject.error == "")
 				{
 					user_id = jsonObject.id;
 					user_name = jsonObject.Name;
 					uid = jsonObject.uid;
+					// Save the name and uid into local storage for future calls/
 					localStorage.setItem("Name", user_name);
 					localStorage.setItem("uid", uid);
-		
+					// Move us to the main events page.
 					window.location.href = "success.html";
 				} else {
+					// If there was an error, display it (e.g. No user 'bop' exists)
 					document.getElementById("result").innerHTML = jsonObject.error;
 				}
 			}
 		};
+		// Sends the JSON to the php file and exectutes function above when a response is received.
 		xhr.send(jsonPayload);
 	}
 	catch(err)
@@ -53,7 +68,7 @@ function doLogin()
  }
  function createAccount()
 {
-
+	// Similar to login, obtain values and send JSON to php.
 	var login = document.getElementById("createName").value;
 	var password = document.getElementById("createPassword").value;
 	var email = document.getElementById("email").value;
@@ -74,6 +89,7 @@ function doLogin()
 				var jsonObject = JSON.parse( xhr.responseText );
 				if(jsonObject.error == "")
 				{
+					// This can change just placeholder.
 					window.location.href = "created.html";
 				} else 
 				{
@@ -105,11 +121,17 @@ function returnPublicEvent()
 				var jsonObject = JSON.parse( xhr.responseText );
 				if(jsonObject.error == "")
 				{
+					// Results is a JSON Array {results:[Name, Desc, Type, ...]}
 					var results = jsonObject.results;
+					// Go through results and form a new row in the table view you see in the events page.
 					for(var i = 0 ; i < results.length; i++)
 					{
+						// Everything here is building up the HTML that goes inside of the table body (in publicEvents look at the id publicView)
 						var res = `results${i+1}`;
+						// Start a row.
 						list += "<tr>";
+						//<td> is the columns.
+						// We need the index here because its how I get the information from local storage.
 						list += `<td>${i+1}</td>`;
 						list += `<td>${results[i].EventName}</td>`;
 						list += `<td>${results[i].Description}</td>`;
@@ -117,11 +139,13 @@ function returnPublicEvent()
 						list += `<td>${results[i].EventDate}</td>`;
 						list += `<td>${results[i].EventStart}</td>`;
 						list += `<td>${results[i].EventEnd}</td>`;
+						// Just setting up a button that when clicked retreives the index it's in to open up that information page with the location.
 						list += `<td>
 						<button type="button" id="${i+1}" class="viewButton" 
 						onclick="openModal(document.getElementById('publicView').rows[${i}].cells[0].innerText);">View this Event
 						</button>`;
 						list += "</tr>";
+						// Save results in local storage.
 						localStorage.setItem(res, JSON.stringify(results[i]));
 					}
 					document.getElementById("publicView").innerHTML = list;
@@ -142,13 +166,18 @@ function returnPublicEvent()
 var modal; 
 function openModal(row)
 {
-	
+	// This is the black transparent background when you click View This Event, holds the white box inside of it with the location information.
 	const modal_container = document.getElementById('container');
+	// Save the current state of the html (it's empty), this is just used to clean out the container of the informatiopn.
 	localStorage.setItem("currstate", modal_container.innerHTML);
 	res = "results"+row;
+	// Same results array as returnPublicEvent();
 	results = JSON.parse(localStorage.getItem(res));
+	//This enables the .css which allows us to see our focused Event View.
 	modal_container.classList.add('show');
 	modal = document.getElementById('left');
+	// the create element is equivalent to adding <h1> EventName </h1 > basically, its just an easier way to update the HTML.
+	// Essentially everything from here is forming the Event View you see (when you click View this event)
 	var header = document.createElement("h1");
 	var text = document.createTextNode(results.EventName);
 	
@@ -159,7 +188,10 @@ function openModal(row)
 	var date = document.createElement("p");
 	var dateText = document.createTextNode(`${results.EventDate} - From ${results.EventStart} To ${results.EventEnd}`);
 	date.appendChild(dateText);
+	// Calls php which returns to us the location of the event. If it's empty, event page is empty.
 	getLocation(results.Eventid);
+	// Why do we setTimeout? Because since js isn't sequential we can do all this work below and the location will not be in local storage
+	// even though we called getLocation before it. This is why when you click View This Event it hands for a fraction of a second.
 	setTimeout(function(){
 		var locJ = localStorage.getItem(`location${results.Eventid}`);
 		console.log(locJ)
@@ -193,6 +225,7 @@ function openModal(row)
 }
  function getLocation(eid)
 {
+	// calls locatedAt.php. 
 	var tmp = {event_id:eid};
 	var jsonPayload = JSON.stringify( tmp );
 	var xhr = new XMLHttpRequest();
@@ -218,6 +251,8 @@ function openModal(row)
 	{
 	}
 }
+// This is the Google API code. Not %100 sure on the technicals of it, but it works. 
+// Any changes we would make are the map and marker const. 
 var map;
 function initMap(eid) {
 	var loc = JSON.parse(localStorage.getItem(`location${eid}`));
@@ -235,7 +270,7 @@ function initMap(eid) {
 	});
   
 }
-
+// when you click close on the events, it closes the modal_container, and resets the container back to that currstate from before.
 function closeModal(){
 	const modal_container = document.getElementById('container');
 	modal_container.classList.remove('show');
