e49103e1b5088d2f59fd93a0dedf8e11d981a790
neomagic100 <neomagic100@gmail.com>
2022-04-03 13:32:57 -0400

    Changing Adding University
    Making addUniversity.html and createUniversity.php

--- LAMP/API/createUniversity.php
+++ LAMP/API/createUniversity.php
@@ -1,32 +1,3 @@
-<html>
-    <head> 
-		<link href="css/styles.css" rel="stylesheet">	
-		<script type="text/javascript" src="js/main.js"></script>
-		<link href="https://fonts.googleapis.com/css?family=Ubuntu" rel="stylesheet">
-        <?php header('Access-Control-Allow-Origin: https://www.goldenknights.systems/');
-                header("Access-Control-Allow-Credentials: true");
-                header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
-                header('Access-Control-Max-Age: 1000');
-                header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token , Authorization');
-                include 'dbconfig.php'; ?>
-	</head>
-
-    <body>
-        <form method="post">
-        <input type="text" id="createUniName" placeholder="UNIVERSITY NAME" /><br /> 
-        <br/>
-        <input type="text" id="createNumStudents" placeholder="ESTIMATED NUMBER OF STUDENTS" /><br /> 
-        <br/>
-        <input type="text" id="createUniAddr" placeholder="ADDRESS" /><br /> 
-        <br/>
-        <input type="text" id="createUniDescription" placeholder="DESCRIPTION"/><br />
-        <br/>
-        <button type="button" id="createUniButton" class="buttons4" onclick="createUniversity()">Create University</button>
-        <div class="resdiv">
-            <span id="resultCreate" class="result"></span>
-        </div>
-        </form>
-
     <?php
         
         $inData = getRequestInfo();
@@ -34,7 +5,10 @@
         $conn = new mysqli($db_server, $db_user, $db_password, $db_name, $db_port); 
         if (!$conn) {
             die("Connection failed: " . mysqli_connect_error());
-        }   
+        }
+        else {
+            $conn->prepare("INSERT INTO University (uni_name, location, num_students, descrip) VALUES (?,?,?,?)");
+        }
         
 
         function getRequestInfo()
