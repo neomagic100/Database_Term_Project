8473c1d47758ffc8d6df3e7330346e6e538fa505
neomagic100 <neomagic100@gmail.com>
2022-04-03 18:08:04 -0400

    Update createUniversity.php

--- LAMP/API/createUniversity.php
+++ LAMP/API/createUniversity.php
@@ -16,11 +16,20 @@
             $stmt->bind_param("ssis", $sanitizedUniName, $sanitizedAddress, $sanitizedNumStudents, $sanitizedDescription);
             $stmt->execute();
             $result = $stmt->get_result();
+            returnWithInfo($result->error);
             $stmt->close();
             $conn->close();
         }
-        
-
+    function sendResultInfoAsJson($obj)
+    {
+        header('Content-type: application/json');
+        echo $obj;
+    }
+    function returnWithInfo($results)
+    {
+        $retValue = '{"error":"' . $results . '"}';
+        sendResultInfoAsJson($retValue);
+    }
         function getRequestInfo()
         {
             return json_decode(file_get_contents('php://input'), true);
