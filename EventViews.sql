ec3f85822d2e1f5f5c751df4a9d4196b146b78bb
neomagic100 <neomagic100@gmail.com>
2022-04-04 17:07:20 -0400

    Update EventViews.sql

--- EventViews.sql
+++ EventViews.sql
@@ -2,7 +2,7 @@ USE UniversityEvents;
 
 -- Dates formatted as Wed, Mar 31, 2022
 -- Times formatted as 11:46 AM
-
+DROP VIEW RSOEventView;
 -- View to see public events, date and time formatted, ordered chronologically
  CREATE VIEW PublicEventView AS
 	SELECT event_id, event_name, descrip, DATE_FORMAT(event_date, '%a, %b %d, %Y') AS event_date,
@@ -15,20 +15,20 @@ USE UniversityEvents;
 
 -- View to see private events, date and time formatted, ordered chronologically
 CREATE VIEW PrivateEventView AS
-    SELECT event_id, event_name, descrip, DATE_FORMAT(event_date, '%a, %b %d, %Y') AS event_date,
+    SELECT E.event_id, event_name, C.uni_id, descrip, DATE_FORMAT(event_date, '%a, %b %d, %Y') AS event_date,
 		TIME_FORMAT(event_start, '%h:%i %p') AS event_start, 
         TIME_FORMAT(event_end, '%h:%i %p') AS event_end
-    FROM PrivateEvents
+    FROM PrivateEvents E, Creates_PrivateEvent C
     INNER JOIN Events USING (event_id)
     WHERE is_published = 1
     ORDER BY Events.event_date, Events.event_start;
 
 -- -- View to see RSO events, date and time formatted, ordered chronologically
 CREATE VIEW RSOEventView AS
-    SELECT event_id, event_name, descrip, DATE_FORMAT(event_date, '%a, %b %d, %Y') AS event_date,
+    SELECT E.event_id, event_name, C.rso_id, descrip, DATE_FORMAT(event_date, '%a, %b %d, %Y') AS event_date,
 		TIME_FORMAT(event_start, '%h:%i %p') AS event_start, 
         TIME_FORMAT(event_end, '%h:%i %p') AS event_end
-    FROM RSOEvents
+    FROM RSOEvents E, Creates_RSOEvent C
     INNER JOIN Events USING (event_id)
     WHERE is_published = 1
     ORDER BY Events.event_date, Events.event_start;
