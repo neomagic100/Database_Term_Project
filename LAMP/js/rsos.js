2b208f7d050342852d6e9bbf23e037efb6681282
neomagic100 <neomagic100@gmail.com>
2022-04-06 17:16:30 -0400

    Update rsos.js

--- LAMP/js/rsos.js
+++ LAMP/js/rsos.js
@@ -2,6 +2,60 @@
 var urlBase = 'https://www.goldenknights.systems/API';
 var extension = 'php';
 
+function returnRSOs_ActiveMember(){
+    var url = urlBase + '/rsosActiveMember.' + extension;
+	var list ="";
+	var xhr = new XMLHttpRequest();
+	xhr.open("GET", url, true);
+	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+	try
+	{
+		xhr.onreadystatechange = function() 
+		{
+			if (this.readyState == 4 && this.status == 200) 
+			{
+				var jsonObject = JSON.parse( xhr.responseText );
+				if(jsonObject.error == "")
+				{
+					// Results is a JSON Array {results:[Name, Desc, Type, ...]}
+					var results = jsonObject.results;
+					// Go through results and form a new row in the table view you see in the events page.
+					for(var i = 0 ; i < results.length; i++)
+					{
+						// Everything here is building up the HTML that goes inside of the table body (in publicEvents look at the id publicView)
+						var res = `results${i+1}`;
+						// Start a row.
+						list += "<tr>";
+						//<td> is the columns.
+						// We need the index here because its how I get the information from local storage.
+						list += `<td>${i+1}</td>`;
+						list += `<td>${results[i].RSOName}</td>`;
+						list += `<td>${results[i].RSOType}</td>`;
+						
+                        // TODO CHANGE
+						// Just setting up a button that when clicked retreives the index it's in to open up that information page with the location.
+						list += `<td>                       
+						<button type="button" id="${i+1}" class="viewButton" 
+						onclick="openModal(document.getElementById('publicView').rows[${i}].cells[0].innerText);">View this Event
+						</button>`;
+						list += "</tr>";
+						
+						// Save results in local storage.
+						localStorage.setItem(activerssoname, JSON.stringify(results[i]));
+					}
+					document.getElementById("rsos").innerHTML = list;
+				} 
+				
+			}				
+		};
+		xhr.send(null);
+	}
+	catch(err)
+	{
+		document.getElementById("publicView").innerHTML = err.message;
+	}
+}
+
 // Get a list for a dropdown menu of RSOs
 function getRSOs() {
 	var url = urlBase + '/rsosList.' + extension;
