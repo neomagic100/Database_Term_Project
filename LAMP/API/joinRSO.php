906d5ed4c03acb16b216b41b3a51a8d9db20d87f
neomagic100 <neomagic100@gmail.com>
2022-04-07 14:23:03 -0400

    Update joinRSO.php

--- LAMP/API/joinRSO.php
+++ LAMP/API/joinRSO.php
@@ -7,12 +7,20 @@
     header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token , Authorization');
 
     $uid = parseInt(localStorage.getItem('uid'));
+    $inData = getRequestInfo();
+    $rso_id = $inData['rsoid'];
 
     $conn = new mysqli($db_server, $db_user, $db_password, $db_name, $db_port);
     if (!$conn) {
         die("Connection failed: " . mysqli_connect_error());
     }
     else {
-        
+        $stmt = $conn->prepare("INSERT INTO Member_of (uid, rso_id) VALUES (?,?);");
+        $stmt->bind_param("ii", $uid, $rso_id);
+        $stmt->execute();
+        $result = $stmt->get_result();
+		returnWithError($stmt->error);
+		$stmt->close();
+        $conn->close();
     }
 ?>
