fbae87962ad3f99e80c284a00bed5b157c1c298f
neomagic100 <neomagic100@gmail.com>
2022-04-08 17:38:40 -0400

    Update joinRSO.php
    Added condition to add a user as admin if the number hits 5 and an owner doesn't exist

--- LAMP/API/joinRSO.php
+++ LAMP/API/joinRSO.php
@@ -15,23 +15,68 @@
         die("Connection failed: " . mysqli_connect_error());
     }
     else {
+        // Add user to Member_of that rso
         $stmt = $conn->prepare("INSERT INTO Member_of (uid, rso_id) VALUES (?,?);");
         $stmt->bind_param("ii", $uid, $rso_id);
         $stmt->execute();
         $result = $stmt->get_result();
 		returnWithError($stmt->error);
 		$stmt->close();
+
+        // Check how many users are currently in that RSO
+        $stmt = $conn->prepare("SELECT COUNT(*) FROM Member_of WHERE rso_id = ?;");
+        $stmt->bind_param("i", $rso_id);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $row = $result->fetch_assoc();
+        $num_members = $row["COUNT(*)"];
+        $stmt->close();
+
+        // If the new addition makes 5 people, add 1 to admins and owns
+        if ($num_members == 5)
+        {
+            // Check if there is already an Owner of the RSO
+            $stmt = $conn->prepare("SELECT COUNT(*) FROM Owns WHERE rso_id = ?;");
+            $stmt->bind_param("i", $rso_id);
+            $stmt->execute();
+            $result = $stmt->get_result();
+            $row = $result->fetch_assoc();
+            $ownerExists = $row["COUNT(*)"];
+            $stmt->close();
+
+            // If an owner does not yet exist
+            if ($ownerExists = 0)
+            {
+                // Add the new user to Admins
+                $stmt = $conn->prepare("INSERT INTO Admins SELECT * FROM Users WHERE user_id = ?;");
+                $stmt->bind_param("i", $uid);
+                $stmt->execute();
+                $result = $stmt->get_result();
+                $stmt->close();
+
+                // Add the new Admin to Owns
+                $stmt = $conn->prepare("INSERT INTO Owns (uid, rso_id) VALUES (?,?);");
+                $stmt->bind_param("ii", $uid, $rso_id);
+                $stmt->execute();
+                $result = $stmt->get_result();
+                $stmt->close();
+            }            
+        }  
+
         $conn->close();
     }
+
     function returnWithError($err)
     {
         $retValue = '{"error":"' . $err . '"}';
         sendResultInfoAsJson($retValue);
     }
+
     function getRequestInfo()
     {
         return json_decode(file_get_contents('php://input'), true);
     }
+
     function sendResultInfoAsJson($obj)
     {
         header('Content-type: application/json');
