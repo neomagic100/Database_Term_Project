9d7ce078cdda012bb57c7324e801e1f1f10eccdb
neomagic100 <neomagic100@gmail.com>
2022-04-09 16:42:14 -0400

    Update createRSOEvent.php

--- LAMP/API/createRSOEvent.php
+++ LAMP/API/createRSOEvent.php
@@ -27,6 +27,81 @@
     }
 
     $conn = new mysqli($db_server, $db_user, $db_password, $db_name, $db_port);
+    if (!$conn) {
+        die("Connection failed: " . mysqli_connect_error());
+    }
+    else {
+        // Make sure user is Owner of RSO
+        $stmt = $conn->prepare("SELECT COUNT(*) FROM Owns WHERE uid = ? AND rso_id = ?;");
+        $stmt->bind_param("ii", $uid, $rsoid);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $row = $result->fetch_assoc();
+        $isOwner = $row["COUNT(*)"];
+        $stmt->close();
 
+        if ($isOwner > 0) {
+            // Add location
+            $stmt = $conn->prepare("INSERT INTO Location (lname, latitude, longitude, address) VALUES (?,?,?,?);");
+            $stmt->bind_param("sdds", $lname, $latitude, $long, $addr);
+            $stmt->execute();
+            $result = $stmt->get_result();
+            $stmt->close();
 
+            // Get location id
+            $stmt = $conn->prepare("SELECT LAST_INSERT_ID();");
+            $stmt->execute();
+            $result = $stmt->get_result();
+            $row = $result->fetch_assoc();
+            $lid = $row["LAST_INSERT_ID()"];
+            $stmt->close();
+
+            // add it
+            $stmt = $conn->prepare("CALL addRSOEvent( ?, ?,  ?, ?, ?,?,  ?);");
+            $stmt->bind_param("ssssssi", $eventname, $eventcat, $descrip, $date, $start, $end, $lid);
+            $stmt->execute();
+            $result = $stmt->get_result();
+            $stmt->close();
+
+            //eid
+            $stmt = $conn->prepare("SELECT LAST_INSERT_ID();");
+            $stmt->execute();
+            $result = $stmt->get_result();
+            $row = $result->fetch_assoc();
+            $eid = $row["LAST_INSERT_ID()"];
+            $stmt->close();
+
+            //put in located at
+            $stmt = $conn->prepare("INSERT INTO Located_at (event_id, lid) VALUES (?,?)");
+            $stmt->bind_param("ii", $eid, $lid);
+            $stmt->execute();
+            $result = $stmt->get_result();
+            $stmt->close();
+
+            //put in creates RSO Event
+            $stmt = $conn->prepare("INSERT INTO Creates_PrivateEvent (rso_id, event_id) VALUES (?,?)");
+            $stmt->bind_param("iii", $rsoid, $eid);
+            $stmt->execute();
+            $result = $stmt->get_result();
+            returnWithError($result->error);
+            $stmt->close();
+            $conn->close()
+        }
+        
+    }
+
+    function returnWithError($err)
+    {
+        $retValue = '{"error":"' . $err . '"}';
+        sendResultInfoAsJson($retValue);
+    }
+    function getRequestInfo()
+    {
+        return json_decode(file_get_contents('php://input'), true);
+    }
+    function sendResultInfoAsJson($obj)
+    {
+        header('Content-type: application/json');
+        echo $obj;
+    }
 ?> 
